8a98857c8ef1370ba6bd0f9c870587b85745184a
narvcrist <cris-narvaez@hotmail.com>
2020-11-19 22:36:56 -0500

    Update model evaluation

--- src/app/models/teacher-eval/evaluation.ts
+++ src/app/models/teacher-eval/evaluation.ts
@@ -1,11 +1,14 @@
 import {State} from '../ignug/models.index';
 import {EvaluationType} from '../teacher-eval/models.index';
+import {Catalogue} from '../ignug/models.index';
 import {Teacher} from '../ignug/models.index';
 
 export interface Evaluation{
     id?: number;
     result?: number;
-    evaluation?: EvaluationType;
+    evaluation_type?: EvaluationType;
     teacher?: Teacher;
+    evaluators?: Teacher[];
     state?: State;
+    status?: Catalogue;
 }
